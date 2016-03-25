11376034da1e469dede7b0eefa9566def7221676
李金磊 <652326533@qq.com>
2016-03-25 17:40:38 +0800

    曲面图添加demo

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cd ~/blog/grenlight.github.io
-jekyll --version
 # webpack --config _webpack_configs/gren.config.js
 #webpack --config _webpack_configs/es6.config.js
 bundle exec jekyll serve
